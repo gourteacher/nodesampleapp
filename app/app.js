599f046f2fbb7cf6157e9c38379fbfd997784540
C8H8 <erwtovell@gmail.com>
2021-12-14 14:19:03 -0500

    Added new filetypes to gitignore and replaced "hello world" with my name

--- app/app.js
+++ app/app.js
@@ -3,17 +3,16 @@ var http = require('http')
 var PORT = 6000
 
 http.createServer(function (request, response) {
-   // Send the HTTP header 
+   // Send the HTTP header
    // HTTP Status: 200 : OK
    // Content Type: text/plain
    response.writeHead(200, {'Content-Type': 'text/plain'});
-   
+
    // Send the response body as "Hello World"
-   response.end('Hello World\n');
+   response.end('Ethan Tovell\n');
 
 
 }).listen(PORT);
 
 // Console will print the message
 console.log('Server running at http://127.0.0.1:'+PORT+'/');
-
